4c1fb9f363ae9dba2aadf90f59a84153d9890bb6
Dirk van Oosterbosch <dirk@innovactory.com>
2015-12-02 23:29:58 +0100

    Add examples of filtering and using files as input

--- Basics/README.md
+++ Basics/README.md
@@ -6,3 +6,4 @@ Take from these examples what you need to do.
 
 - examples of simple `input-output`
 - `.json` reading and writing
+- `filter-files` example (searches `.md` files for links)
---
+++ Basics/filter-files/files_input.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+# input_files.py
+
+"""Example 
+"""
+
+import os
+import sys
+import glob
+
+def getFilesFromArguments():
+	"""Get the files delivered on the command line,
+	either as one or multiple file arguments:
+	e.g. `python script.py file1.md file2.md file3.md`
+	or as resulted from a wildcard character syntax:
+	e.g. `python script.py file*.md`
+	"""
+	files = []
+	for arg in sys.argv[1:]:
+		# Glob is needed for a cross platform solution
+		files += glob.glob(arg)
+	#
+	# filter #1
+	# Make sure that all elements are indeed files
+	files = [f for f in files if os.path.exists(f)]
+	#
+	# filter #2
+	# Make sure that the files are not empty
+	files = [f for f in files if os.stat(f).st_size > 0]
+	#
+	return files
+
+
+def main():
+	"""Opens the files deliverd to this script
+	and print the first line.
+	"""
+	#
+	# Get the files given as command line arguments
+	commandLineFiles = getFilesFromArguments()
+	#
+	# Print the first line of each file
+	for file in commandLineFiles:
+		with open(file, 'r') as f:
+			lines = f.readlines()
+			print lines[0]
+	
+if __name__ == '__main__':
+	main()
