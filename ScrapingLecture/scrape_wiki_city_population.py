18518339ae322d981f531f4818231322c7f10385
irlabs <developer@irlabs.nl>
2016-03-31 08:03:51 +0200

    Fixing two more bugs when tr['class'] or th cannot be found

--- ScrapingLecture/scrape_wiki_city_population.py
+++ ScrapingLecture/scrape_wiki_city_population.py
@@ -23,42 +23,46 @@ def main():
 		if c.has_key('cities'):
 			cities = c['cities']
 			pBarCountries.set_description("Processing %s" % country)
-			pBarCities = tqdm(cities[:5], leave=True, nested=True)
+			pBarCities = tqdm(cities, leave=True, nested=True)
 			for city in pBarCities:
 				# Open the city page
 				r = urllib.urlopen(city['url']).read()
 				soup = BeautifulSoup(r, "html.parser")
 				# Get the infobox table
 				infoBox = soup.find('table', class_="infobox")
-				# Get all mergedTopRows
-				mergedTopRows = infoBox.findAll('tr', class_="mergedtoprow")
-				# Find the Population mergedTopRow
-				populationMergedTopRow = None
-				for m in mergedTopRows:
-					text = m.text.strip()
-					if text.startswith("Population"):
-						populationMergedTopRow = m
-						break
-				if populationMergedTopRow:
-					pDict = {}
-					foundPopulation = False
-					for tr in populationMergedTopRow.findNextSiblings('tr'):
-						if 'mergedrow' in tr['class']:
-							populationKey = tr.find('th').text.strip().strip(u'\xa0\u2022\xa0')
-							if populationKey:
-								populationValue = tr.find('td').text.strip()
-								if populationValue:
-									# Add the key and value to a sub - dict
-									pDict[populationKey] = populationValue
-									# Keep track of whether population was found
-									foundPopulation = True
-						else:
+				# If there is a infoBox
+				if infoBox:
+					# Get all mergedTopRows
+					mergedTopRows = infoBox.findAll('tr', class_="mergedtoprow")
+					# Find the Population mergedTopRow
+					populationMergedTopRow = None
+					for m in mergedTopRows:
+						text = m.text.strip()
+						if text.startswith("Population"):
+							populationMergedTopRow = m
 							break
-					if foundPopulation:
-						# increment a counter
-						numberOfCities += 1
-						# Add the info to the city data
-						city['populationInfo'] = pDict
+					if populationMergedTopRow:
+						pDict = {}
+						foundPopulation = False
+						for tr in populationMergedTopRow.findNextSiblings('tr'):
+							if tr.has_attr('class') and 'mergedrow' in tr['class']:
+								th = tr.find('th')
+								if th and th.text:
+									populationKey = th.text.strip().strip(u'\xa0\u2022\xa0')
+									if populationKey:
+										populationValue = tr.find('td').text.strip()
+										if populationValue:
+											# Add the key and value to a sub - dict
+											pDict[populationKey] = populationValue
+											# Keep track of whether population was found
+											foundPopulation = True
+							else:
+								break
+						if foundPopulation:
+							# increment a counter
+							numberOfCities += 1
+							# Add the info to the city data
+							city['populationInfo'] = pDict
 	# Finally: save a file, with all cities by countries
 	with open("city_populations.json", 'w') as outputFile:
 	   json.dump(citiesData, outputFile, indent=2)
